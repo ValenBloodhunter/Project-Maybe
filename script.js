dccc7e911582c55e91ae90fd28fa6d304cc57a54
Debashis <debashis010731@gmail.com>
2026-09-20 18:25:10 +0530

    feat: tested first connection

---
+++ script.js
@@ -0,0 +1,24 @@
+// =========================
+// PROJECT MAYBE
+// Phase 0
+// =========================
+
+
+// This proves that JavaScript
+// is connected correctly.
+
+console.log("Project Maybe initialized.");
+
+
+// Find the status text
+
+const statusText = document.getElementById("statusText");
+
+
+// Small startup effect
+
+setTimeout(() => {
+
+    statusText.textContent = "System operational";
+
+}, 1000);
